34b25e1c0cddf2ac50c5464cee3ff94b0c91e143
Aryanji4201 <aryanprajapati4201@gmail.com>
2026-07-02 11:33:04 +0530

    Complete dashboard redesign

--- app/routes/home.py
+++ app/routes/home.py
@@ -3,7 +3,7 @@ from flask_login import login_required, current_user
 
 from app.models.post import Post
 from app.models.follow import Follow
-
+from app.models.user import User
 print("Home blueprint loaded")
 home = Blueprint("home", __name__)
 
@@ -11,33 +11,17 @@ home = Blueprint("home", __name__)
 @login_required
 def dashboard():
 
-    # posts = Post.query.order_by(
-    #     Post.created_at.desc()
-    # ).all()
-    following = Follow.query.filter_by(
-    follower_id=current_user.id
+    posts = Post.query.order_by(
+        Post.created_at.desc()
     ).all()
 
-    following_ids = [
-    follow.following_id
-    for follow in following
-]
-    following_ids.append(current_user.id)
-
-    posts = Post.query.filter(
-    Post.user_id.in_(following_ids)
-).order_by(
-    Post.created_at.desc()
-).all()
-
+    suggested_users = User.query.filter(
+        User.id != current_user.id
+    ).limit(5).all()
 
     return render_template(
-
         "dashboard.html",
-
         user=current_user,
-
-        posts=posts
-
+        posts=posts,
+        suggested_users=suggested_users
     )
-
